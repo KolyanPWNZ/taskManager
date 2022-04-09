a28749f3316d07334909d06591ef272025fa48d4
Nikolay <krukolnik@gmail.com>
2022-04-09 19:02:15 +0300

    осуществил миграцию с БД

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'taskManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pythonDev',
+        'USER': 'postgres',
+        'PASSWORD': 'ewosh2dozi',
     }
 }
 
